0b6f5db1842f676f459d2986df94caa62b2be0e7
Masirika Constantin <elconstant73@gmail.com>
2025-06-25 18:01:02 +0100

    Mise à jour de la migration pour la table fournisseurs : gestion des types JSON selon la version de MySQL

--- database/migrations/2025_06_19_234958_create_fournisseurs_table.php
+++ database/migrations/2025_06_19_234958_create_fournisseurs_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -18,7 +19,12 @@ return new class extends Migration
             $table->string('phone')->nullable();
             $table->string('email')->nullable();
             $table->text('address')->nullable();
-            $table->jsonb('specialties')->default('[]');
+            if (DB::getDriverName() === 'mysql' && version_compare(DB::select("select version() as v")[0]->v, '8.0.13', '<')) {
+                $table->json('specialties')->nullable(); // pas de default
+            } else {
+                $table->json('specialties')->default(DB::raw('(JSON_ARRAY())'));
+            }
+            
             $table->boolean('is_active')->default(true);
             $table->date('contract_start_date')->nullable();
             $table->date('contract_end_date')->nullable();
